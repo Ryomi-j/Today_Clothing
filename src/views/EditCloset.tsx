532caa8ddfe5c6b4ce78f0c5b57fee5130d8913c
Ryomi-j <ryomi.j78@gmail.com>
2023-04-26 23:14:15 +0900

    add input tag - to upload image file

--- src/views/EditCloset.tsx
+++ src/views/EditCloset.tsx
@@ -4,12 +4,14 @@ import { Modal } from "../components/common/Modal";
 export const EditCloset = () => {
 	return (
 		<div className="flex min-h-[calc(100vh-3.3rem)] pt-16 bg-base-200">
-			<div className="card gap-5 my-6 mx-auto min-w-2/5 bg-base-100 shadow-xl p-7">
+			<div className="card gap-5 my-6 mx-auto py-7 px-14 bg-base-100 shadow-xl">
 				<h2 className="text-4xl font-extrabold text-center pt-5 pb-5">Edit Weekly Closet</h2>
-				<figure className="w-96 h-96 m-auto border-2 rounded-md">
-					<img src="/addImg.svg" alt="Shoes" className="w-1/4" />
+				<figure className="w-96 h-96 m-auto border-2 rounded-md cursor-pointer">
+					<label className="w-1/3 h-1/3 bg-[url('/public/addImg.svg')] bg-no-repeat bg-center bg-contain ">
+						<input type="file" className="hidden" />
+					</label>
 				</figure>
-				<div className="card-body">
+				<div className="card-body max-h-fit">
 					<h3 className=" mb-14 text-3xl font-semibold text-center">Tue</h3>
 					<div className="flex justify-end gap-2">
 						<label htmlFor="my-modal-6" className="btn btn-primary">
