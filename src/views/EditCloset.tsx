61cbc4fba0222fb313a57465c59af677ecb7b6aa
Ryomi-j <ryomi.j78@gmail.com>
2023-04-26 18:47:08 +0900

    edit edit-closet page - each item size

--- src/views/EditCloset.tsx
+++ src/views/EditCloset.tsx
@@ -3,17 +3,16 @@ import { Modal } from "../components/common/Modal";
 
 export const EditCloset = () => {
 	return (
-		<div className="flex w-screen min-h-[calc(100vh-3.3rem)] pt-16 bg-base-200">
-			<div className="card m-auto w-fit h-auto bg-base-100 shadow-xl py-7 px-10">
+		<div className="flex min-h-[calc(100vh-3.3rem)] pt-16 bg-base-200">
+			<div className="card gap-5 my-6 mx-auto min-w-2/5 bg-base-100 shadow-xl p-7">
 				<h2 className="text-5xl text-center pt-5 pb-5">Edit Weekly Closet</h2>
-				<figure className="w-96 h-96 border-2 rounded-md">
+				<figure className="w-96 h-96 m-auto border-2 rounded-md">
 					<img src="/vite.svg" alt="Shoes" />
 				</figure>
 				<div className="card-body">
-					<h3 className="mt-8 mb-8 text-xl text-center">Tue</h3>
-
-					<div className="flex flex-row-reverse">
-						<label htmlFor="my-modal-6" className="btn btn-primary w-1/3">
+					<h3 className=" mb-14 text-4xl text-center">Tue</h3>
+					<div className="flex justify-end gap-2">
+						<label htmlFor="my-modal-6" className="btn btn-primary">
 							Save
 						</label>
 						<Link to="/closet">
@@ -22,7 +21,7 @@ export const EditCloset = () => {
 					</div>
 				</div>
 			</div>
-			<Modal content="내용을 저장하시겠습니까?" btnContent="OK" link="closet" btnContent2="No" />
+			<Modal content="내용을 저장하시겠습니까?" btnContent="Yes" link="closet" btnContent2="No" />
 		</div>
 	);
 };
