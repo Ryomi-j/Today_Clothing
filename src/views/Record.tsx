345d2fa2107810b89e2bd78293e7f00e5a8cdb90
Ryomi-j <ryomi.j78@gmail.com>
2023-05-09 17:04:44 +0900

    change title content and style

--- src/views/Record.tsx
+++ src/views/Record.tsx
@@ -6,7 +6,7 @@ export const Record = () => {
 	return (
 		<div className="flex min-h-[calc(100vh-3.3rem)] pt-16 bg-base-200">
 			<div className="card gap-5 my-8 mx-auto min-w-2/5 bg-base-100 shadow-xl p-7">
-				<h2 className="text-5xl text-center pt-5 pb-5">Your Weekly Closet</h2>
+				<h2 className="text-4xl font-extrabold text-center pt-5 pb-5">Record</h2>
 				<div className="grid grid-rows-4 grid-cols-2 gap-6 justify-center items-center">
 					<ImageFrame content="23년 4월 24일 월요일 15도 흐림" />
 					<ImageFrame content="23년 4월 24일 월요일 15도 흐림" />
