3a07971d7707416b36f6d34f9f72f8fe09d3552e
Ryomi-j <ryomi.j78@gmail.com>
2023-05-09 15:54:38 +0900

    add image preview feature

--- src/views/EditCloset.tsx
+++ src/views/EditCloset.tsx
@@ -1,30 +1,35 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import { ref, uploadBytes } from "firebase/storage";
-import { storage } from "../firebase";
-import { useRecoilValue } from "recoil";
-import { userInfo } from "../store/user";
-import { v4 } from "uuid";
+import { useEffect, useState } from "react";
+import { Modal } from "../components/common/Modal";
 
 export const EditCloset = () => {
+	const [imgUrl, setImgUrl] = useState<undefined | string>("/public/addImg.svg");
 	const [imgUpload, setImgUpload] = useState<undefined | File>(undefined);
-	const userUid = useRecoilValue(userInfo).uid;
-	console.log(userUid);
 
-	const uploadImg = () => {
-		if (!imgUpload) return;
-		const imgRef = ref(storage, `imgs/${userUid + imgUpload.name + v4()}`);
-		uploadBytes(imgRef, imgUpload).then(() => {
-			alert("img uploaded");
-		});
+	const getImgUrl = (file: File) => {
+		const url = URL.createObjectURL(file);
+		setImgUrl(url);
 	};
+
+	useEffect(() => {
+		if (imgUrl) {
+			const label = document.querySelector("#label");
+			if (label instanceof HTMLElement) {
+				label.style.backgroundImage = `url(${imgUrl})`;
+				if(imgUrl !== "/public/addImg.svg"){
+					label.style.width = "100%"
+					label.style.height = "100%"
+				}
+			}
+		}
+	}, [imgUrl]);
 
 	return (
 		<div className="flex min-h-[calc(100vh-3.3rem)] pt-16 bg-base-200">
 			<div className="card gap-5 my-6 mx-auto py-7 px-14 bg-base-100 shadow-xl">
 				<h2 className="text-4xl font-extrabold text-center pt-5 pb-5">Edit Weekly Closet</h2>
 				<figure className="w-96 h-96 m-auto border-2 rounded-md cursor-pointer">
-					<label className="w-1/3 h-1/3 bg-[url('/public/addImg.svg')] bg-no-repeat bg-center bg-contain ">
+					<label id="label" className="w-1/3 h-1/3 bg-no-repeat bg-center bg-contain ">
 						<input
 							type="file"
 							className="hidden"
@@ -32,6 +37,7 @@ export const EditCloset = () => {
 								const file = e.target.files?.[0];
 								if (file) {
 									setImgUpload(file);
+									getImgUrl(file);
 								}
 							}}
 						/>
@@ -40,7 +46,7 @@ export const EditCloset = () => {
 				<div className="card-body max-h-fit">
 					<h3 className=" mb-14 text-3xl font-semibold text-center">Tue</h3>
 					<div className="flex justify-end gap-2">
-						<label htmlFor="my-modal-6" className="btn btn-primary" onClick={uploadImg}>
+						<label htmlFor="my-modal-6" className="btn btn-primary" >
 							Save
 						</label>
 						<Link to="/closet">
@@ -49,6 +55,11 @@ export const EditCloset = () => {
 					</div>
 				</div>
 			</div>
+			<Modal
+				content="이미지가 성공적으로 저장되었습니다. Closet 페이지로 이동합니다."
+				btnContent="OK"
+				link={`closet`}
+			/>
 		</div>
 	);
 };
