138b371d043aaf096f99beae3be9fa9878fc9e2c
Ryomi-j <ryomi.j78@gmail.com>
2023-05-11 12:11:25 +0900

    erase unnecessary code

--- src/store/post.ts
+++ src/store/post.ts
@@ -20,7 +20,6 @@ export const postData = selector<Post[]>({
 			const posts = collection(db, "post");
 			const postItems = await getDocs(posts);
 			const post = postItems.docs.map((doc) => doc.data() as Post);
-            console.log(post)
 			return post || [];
 		} catch (error) {
 			console.error(error);
