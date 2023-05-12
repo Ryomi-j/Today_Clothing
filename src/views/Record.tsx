e138bfc1ea06a3d636bae6fc00eeae18fecb2625
Ryomi-j <ryomi.j78@gmail.com>
2023-05-12 11:39:39 +0900

    erase unnecessary code

--- src/views/Record.tsx
+++ src/views/Record.tsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { ImageFrame } from "../components/common/ImageFrame";
 import { MdNavigateBefore } from "react-icons/md";
 
 export const Record = () => {
@@ -8,13 +7,7 @@ export const Record = () => {
 			<div className="card gap-5 my-8 mx-auto min-w-2/5 bg-base-100 shadow-xl p-7">
 				<h2 className="text-4xl font-extrabold text-center pt-5 pb-5">Record</h2>
 				<div className="grid grid-rows-4 grid-cols-2 gap-6 justify-center items-center">
-					<ImageFrame content="23년 4월 24일 월요일 15도 흐림" />
-					<ImageFrame content="23년 4월 24일 월요일 15도 흐림" />
-					<ImageFrame content="23년 4월 24일 월요일 15도 흐림" />
-					<ImageFrame content="23년 4월 24일 월요일 15도 흐림" />
-					<ImageFrame content="23년 4월 24일 월요일 15도 흐림" />
-					<ImageFrame content="23년 4월 24일 월요일 15도 흐림" />
-					<ImageFrame content="23년 4월 24일 월요일 15도 흐림" />
+
 				</div>
 				<div className="flex flex-row-reverse mt-7">
 					<Link to="/closet">
@@ -24,7 +17,6 @@ export const Record = () => {
 					</Link>
 				</div>
 			</div>
-			{/* <Modal content="오늘 당신의 의상을 공유하시겠습니까?" btnContent="OK" btnContent2="Cancel"/> */}
 		</div>
 	);
 };
