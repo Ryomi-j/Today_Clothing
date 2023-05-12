d47833263405db2f5c4c6fc3ecb41217b74d96d9
Ryomi-j <ryomi.j78@gmail.com>
2023-05-12 14:52:38 +0900

    resize record frame and only show back button when user has no record data

--- src/views/Record.tsx
+++ src/views/Record.tsx
@@ -21,26 +21,36 @@ export const Record = () => {
 			today.getDate() + ((1 + 7 - today.getDay()) % 7)
 		);
 		// 1684594800000
-		// const posts = userPosts.filter((post) => post.date !== undefined && post.date < 1684594800000);
-		const posts = userPosts.filter((post) => post.date !== undefined && post.date < nextMonday.getTime());
+		const posts = userPosts.filter((post) => post.date !== undefined && post.date < 1684594800000);
+		// const posts = userPosts.filter((post) => post.date !== undefined && post.date < nextMonday.getTime());
 		return posts;
 	}, []);
 
 	return (
 		<div className="flex min-h-[calc(100vh-3.3rem)] pt-16 bg-base-200">
-			<div className="card gap-5 my-8 mx-auto min-w-2/5 min-h-min bg-base-100 shadow-xl p-7">
-				<h2 className="text-4xl font-extrabold text-center pt-5 pb-5">Record</h2>
-				{userRecords.length === 0 ? (
+			{userRecords.length === 0 ? (
+				<div className="card gap-5 my-auto mx-auto min-w-2/5 h-80 bg-base-100 shadow-xl p-7">
+					<h2 className="text-4xl font-extrabold text-center pt-5 pb-5">Record</h2>
 					<div className="grid gap-6 mx-6 justify-center items-center">
 						<div className="flex items-center gap-2 mt-10 font-extrabold">
 							No clothing records found <RiEmotionSadLine />
 						</div>
 					</div>
-				) : (
-					userRecords.map((post) => {
-						let timeStamp = new Date(Number(post.date));
-						return (
-							<div className={`grid grid-cols-2 gap-6 justify-center items-center`}>
+					<div className="flex flex-row-reverse mt-7">
+						<Link to="/closet">
+							<button className="btn">
+								<MdNavigateBefore className="text-xl" /> back
+							</button>
+						</Link>
+					</div>
+				</div>
+			) : (
+				<div className="card gap-5 my-8 mx-auto min-w-2/5 min-h-min bg-base-100 shadow-xl p-7">
+					<h2 className="text-4xl font-extrabold text-center pt-5 pb-5">Record</h2>
+					<div className={`grid grid-cols-2 gap-6 justify-center items-center`}>
+						{userRecords.map((post) => {
+							let timeStamp = new Date(Number(post.date));
+							return (
 								<ImageFrame
 									content={`${timeStamp.getFullYear()}년 ${timeStamp.getMonth() + 1}월 ${timeStamp.getDate()}일 ${
 										days[timeStamp.getDay()]
@@ -48,18 +58,11 @@ export const Record = () => {
 									src={post.imgUrl}
 									date={timeStamp}
 								/>
-							</div>
-						);
-					})
-				)}
-				<div className="flex flex-row-reverse mt-7">
-					<Link to="/closet">
-						<button className="btn">
-							<MdNavigateBefore className="text-xl" /> back
-						</button>
-					</Link>
+							);
+						})}
+					</div>
 				</div>
-			</div>
+			)}
 		</div>
 	);
 };
