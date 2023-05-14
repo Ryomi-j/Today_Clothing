12248c0404c8a8965e9d190b9283359709c817c9
Ryomi-j <ryomi.j78@gmail.com>
2023-05-14 22:44:42 +0900

    add post item to talk page

--- src/views/Talk.tsx
+++ src/views/Talk.tsx
@@ -1,18 +1,29 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import { useRecoilValue } from "recoil";
+import { Post, postData } from "../store/post";
+import { useEffect, useState } from "react";
 
 export const Talk = () => {
+	const postItems = useRecoilValue(postData);
+	const [posts, setPosts] = useState<Post[] | undefined>(undefined);
+
+	useEffect(() => {
+		const sharedPosts = postItems.filter((post) => post.isPost === true);
+		setPosts(sharedPosts);
+	}, []);
+	console.log(posts);
 	return (
-		<div className="flex flex-col items-center w-screen min-h-[calc(100vh-3.3rem)] pt-16 bg-base-200">
+		<div className="flex flex-col items-center min-h-[calc(100vh-3.3rem)] pt-16 bg-base-200">
 			<Carousel
 				autoPlay
 				showThumbs={false}
 				interval={6000}
 				showStatus={false}
 				infiniteLoop={true}
-				className="flex justify-center max-w-1500px"
+				className="flex justify-center max-w-screen-2xl"
 			>
-				<div>
+				<div className="overflow-hidden">
 					<div className="detail p-4 text-left text-4xl">
 						<p className="text-red-600 text-4xl">Early Bird Sale</p>다가온 여름, 먼저 준비하세요
 					</div>
@@ -25,7 +36,23 @@ export const Talk = () => {
 					<img src="https://firebasestorage.googleapis.com/v0/b/today-clothing.appspot.com/o/add%2Fman-g97c9fcec5_1920.jpg?alt=media&token=1ca0e230-7b37-48d6-a4f8-9738524168f6" />
 				</div>
 			</Carousel>
-			<div></div>
+			<div className="grid grid-cols-3 gap-6 my-10 justify-center items-center max-w-screen-2xl">
+				{posts?.map((post) => {
+					return (
+						<div className="card card-compact bg-base-100 shadow-xl cursor">
+							<figure className="mx-10 mt-10 max-h-72 overflow-hidden object-cover">
+								<img src={post.imgUrl} alt={`${post.uid}-${post.date}-clothing info`} className="rounded-xl" />
+							</figure>
+							<div className="card-body flex-row flex-wrap items-center text-center">
+								<div className="badge badge-md badge-primary badge-outline">#{post.location}</div>
+								<div className="badge badge-md badge-secondary badge-outline">#{post.weather}</div>
+								<div className="badge badge-md badge-outline">#{`${post.degree}C°`}</div>
+								<div className="badge badge-md badge-accent badge-outline">#{`습도_${post.humidity}%`}</div>
+							</div>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
