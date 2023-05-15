c663c842543d1de72116e2be7b0f79aa9d0ad400
Ryomi-j <ryomi.j78@gmail.com>
2023-05-15 13:49:16 +0900

    make weekDatesState atom

--- src/store/editItem.ts
+++ src/store/editItem.ts
@@ -9,3 +9,8 @@ export const nextMondayState = atom<Date | undefined>({
 	key: "nextMonday",
 	default: undefined,
 });
+
+export const weekDatesState = atom({
+	key: "weekDatesState",
+	default: [] as Date[],
+  });
