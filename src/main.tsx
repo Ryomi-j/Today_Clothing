82e7ebf81d1e2f52d0fb61675be3aa6a8f0f3965
Ryomi-j <ryomi.j78@gmail.com>
2023-05-15 16:19:09 +0900

    wrap App component with React.Suspense to fix error

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { RecoilRoot } from 'recoil';
+import { RecoilRoot } from "recoil";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -7,7 +7,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<RecoilRoot>
-			<App />
+			<React.Suspense fallback={<div>Loading...</div>}>
+				<App />
+			</React.Suspense>
 		</RecoilRoot>
 	</React.StrictMode>
 );
