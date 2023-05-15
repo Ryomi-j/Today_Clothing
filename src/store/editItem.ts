0c963a63f8f0e3c6699dbe1c100bd09205ae66de
Ryomi-j <ryomi.j78@gmail.com>
2023-05-15 12:04:08 +0900

    make nextMonday atom

--- src/store/editItem.ts
+++ src/store/editItem.ts
@@ -1,6 +1,11 @@
 import { atom } from "recoil";
 
 export const selectedDate = atom({
-    key: 'selectedDate',
-    default: 0
-})
+	key: "selectedDate",
+	default: 0,
+});
+
+export const nextMonday = atom<Date | undefined>({
+	key: "nextMonday",
+	default: undefined,
+});
