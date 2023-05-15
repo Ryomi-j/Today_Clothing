bdf671b4288593c78f671650049135e64465386a
Ryomi-j <ryomi.j78@gmail.com>
2023-05-15 18:24:00 +0900

    delete unnecessary code

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -91,9 +91,6 @@ const provider = new GoogleAuthProvider();
 export const loginGoogle = () => {
 	return signInWithPopup(auth, provider)
 		.then((result) => {
-			// This gives you a Google Access Token. You can use it to access the Google API.
-			// const credential = GoogleAuthProvider.credentialFromResult(result);
-			// const token = credential?.accessToken;
 			const user = result.user;
 			const userData = {
 				uid: user.uid,
