f41fd8b4dac344ddd733a43b10d2d26d12b32b42
Ryomi-j <ryomi.j78@gmail.com>
2023-05-16 23:59:11 +0900

    add media query break points

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
+		screens: {
+			'xxxs': '1px',
+			'xxs': '300px',
+			'xs': '475px',
+			...defaultTheme.screens,
+		  },
 	},
 	plugins: [require("daisyui")],
 };
