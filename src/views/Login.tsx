29da3cac6bccc44f750b75a8d3f0d74452c9bf66
Ryomi-j <ryomi.j78@gmail.com>
2023-05-17 00:00:19 +0900

    add media query to Login component

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -49,34 +49,34 @@ export const Login = () => {
 			<GetGeoInfo />
 			<div className="hero min-h-[calc(100vh-3.3rem)] pt-16 bg-base-200">
 				<div className="card flex-shrink-0 shadow-2xl bg-base-100">
-					<div className="card-body p-14 gap-5">
-						<h2 className="text-center text-5xl font-bold">Login</h2>
-						<div className="form-control flex-row justify-center mt-16 ">
-							<label className="label w-24  pr-5">
-								<span className="label-text font-bold">ID</span>
+					<div className="card-body px-3 md:p-14 md:gap-5">
+						<h2 className="text-center text-xl xs:text-4xl md:text-5xl font-bold">Login</h2>
+						<div className="form-control flex-row justify-center items-center mt-4 xs:mt-8 md:mt-16 ">
+							<label className="label md:w-24  pr-5">
+								<span className="label-text font-bold h-fit">ID</span>
 							</label>
-							<input type="text" placeholder="id" className="input input-bordered" ref={idInput} />
+							<input type="text" placeholder="id" className="input input-bordered w-36 h-8 xs:w-52 xs:h-12 md:auto" ref={idInput} />
 						</div>
-						<div className="form-control  flex-row justify-center">
-							<label className="label w-24 pr-3 ">
+						<div className="form-control flex-row justify-center md:w-auto">
+							<label className="label md:w-24 pr-3 ">
 								<span className="label-text font-bold">PW</span>
 							</label>
-							<input type="password" placeholder="password" className="input input-bordered" ref={pwInput} />
+							<input type="password" placeholder="password" className="input input-bordered w-36 h-8 xs:w-52 xs:h-12 md:auto" ref={pwInput} />
 						</div>
 						<div className="flex mt-6 items-center">
-							<label className="btn btn-primary w-1/3 m-auto" id="login" onClick={handleLogin}>
+							<label className="btn btn-primary w-1/3 m-auto btn-sm text-xs xs:btn-md" id="login" onClick={handleLogin}>
 								Login
 							</label>
 						</div>
 						<label className="label justify-end mt-3">
-							<Link to="/sign-up" className="label-text-alt link link-hover mt-2 text-base">
+							<Link to="/sign-up" className="label-text-alt link link-hover md:mt-2 text-base">
 								Create Account
 							</Link>
 						</label>
 						<div className="divider"></div>
 						<div className="flex flex-col gap-7">
 							<p className="text-center mt-4">Social login can save your valuable time.</p>
-							<button className="btn w-1/3 m-auto" onClick={googleLogin}>
+							<button className="btn w-1/3 m-auto btn-sm text-xs xs:btn-md" onClick={googleLogin}>
 								Google
 							</button>
 						</div>
