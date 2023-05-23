8b3dc1288e7b2337d6cadd5c1d1814a66967f548
Ryomi-j <ryomi.j78@gmail.com>
2023-05-23 13:54:20 +0900

    erase unnecessary code ans edit User interface

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,25 +1,12 @@
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../firebase";
-import { atom, selector } from "recoil";
-
-export interface Data {
-	createdAt: string;
-	date: string;
-	humidity: string;
-	isPublic: boolean;
-	location: string;
-	src: string;
-	tag: string;
-	temperature: number;
-	weather: string;
-}
+import { atom } from "recoil";
 
 export interface User {
+	createdAt: string;
+	name: string;
 	uid: string;
-	data?: Data;
+	userId: string;
 }
 
-// 로그인 여부 T/F
 export const userState = atom({
 	key: "isLogin",
 	default: false,
@@ -28,20 +15,4 @@ export const userState = atom({
 export const userInfo = atom<User | null>({
 	key: "userInfo",
 	default: null,
-});
-
-export const userData = selector<User[]>({
-	key: "userData",
-	get: async () => {
-		try {
-			const users = collection(db, "users");
-			const usersInfo = await getDocs(users);
-			const user = usersInfo.docs.map((doc) => doc.data() as User);
-			console.log(user);
-			return user || [];
-		} catch (error) {
-			console.error(error);
-			return [];
-		}
-	},
 });
