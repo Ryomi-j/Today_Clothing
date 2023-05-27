404f4746d076cc95962b79133d1363d591a5f12c
Ryomi-j <ryomi.j78@gmail.com>
2023-05-27 16:37:25 +0900

    trim Modal component - erase unnecessary code

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -5,14 +5,13 @@ type ModalProps = {
 	link?: string;
 	btnContent: string;
 	btnContent2?: string;
-	checked?: boolean;
 	handleClick?: React.MouseEventHandler<HTMLLabelElement>;
 };
 
-export const Modal = ({ content, link, btnContent, btnContent2, checked, handleClick }: ModalProps) => {
+export const Modal = ({ content, link, btnContent, btnContent2, handleClick }: ModalProps) => {
 	return (
 		<>
-			<input type="checkbox" id="my-modal-6" className="modal-toggle" checked={checked} />
+			<input type="checkbox" id="my-modal-6" className="modal-toggle" />
 			<div className="modal modal-bottom sm:modal-middle">
 				<div className="modal-box">
 					<h3 className="font-bold text-lg">{content}</h3>
