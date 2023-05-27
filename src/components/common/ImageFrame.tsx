26739d51a8700904b9c0f3f6b75937555d44fa70
Ryomi-j <ryomi.j78@gmail.com>
2023-05-27 16:38:56 +0900

    edit ImageFrame component - show edit and delete icon when the screen size is smaller

--- src/components/common/ImageFrame.tsx
+++ src/components/common/ImageFrame.tsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { selectedDate } from "../../store/date";
 import { Post } from "../../store/post";
+import { GrEdit } from "react-icons/gr";
+import { MdDeleteOutline } from "react-icons/md";
 
 interface ImageFrameDataType {
 	content: string;
@@ -17,30 +19,35 @@ export const ImageFrame = ({ content, post, deleteBtn, date, prevPage, handleCli
 
 	return (
 		<div className="card card-compact w-32 md:w-52 md:h-52 xl:w-96 xl:h-auto bg-base-100 shadow-xl">
-			<figure className="w-32 h-28 md:w-52 md:h-40 xl:w-96 xl:h-96 max-w-96 max-h-96 bg-base-200 cursor-pointer rounded-t-2xl object-contain">
+			<figure className="w-32 h-28 md:w-52 md:h-40 xl:w-96 xl:h-96 max-w-96 max-h-96 bg-base-200 cursor-pointer rounded-t-2xl overflow-hidden object-cover">
 				<img src={post?.imgUrl} alt={`${date?.toString().slice(0, 15)} clothing image`} />
 			</figure>
-			<div className=" md:h-20 xl:h-28 md:p-3">
-				<h3 className="card-title justify-center gap-1 leading-3 text-[10px] md:text-sm xl:text-lg whitespace-pre-wrap text-center">
+			<div className="md:h-20 xl:h-28 md:p-3">
+				<h3 className="card-title justify-center gap-1 text-xs xl:text-lg whitespace-pre-wrap text-center">
 					{content}
 				</h3>
-				<div className="flex gap-2 justify-end">
-					<div className="card-actions justify-end mt-[-0.3rem] xl:mt-0">
+				<div className="flex md:gap-2 justify-end items-center mr-2">
+					<div className="card-actions justify-end xl:mt-0">
 						<Link to={`/editPost?prevPage=${prevPage}&content=${encodeURIComponent(content)}`}>
 							{date && (
 								<button
-									className="bg-primary text-white font-medium rounded-md text-[7px] p-1 mr-2 leading-none md:btn md:btn-primary text-xs md:btn-xs xl:btn-sm xl:p-2"
+									className="flex items-center font-medium rounded-md text-[7px] leading-none btn-xs md:btn md:btn-primary text-xs md:btn-xs xl:btn-sm xl:p-2"
 									onClick={() => setDate(date.getTime())}
 								>
-									edit
+									<span className="hidden md:block ">EDIT</span>
+									<GrEdit className="md:hidden" />
 								</button>
 							)}
 						</Link>
 					</div>
 					{deleteBtn && handleClickPost && post && (
 						<div className="form-control" onClick={() => handleClickPost(post)}>
-							<label htmlFor="my-modal-6" className="btn font-medium rounded-md text-[7px] p-1 mr-2 leading-none text-xs md:btn md:btn-xs xl:btn-sm xl:p-2">
-								delete
+							<label
+								htmlFor="my-modal-6"
+								className="flex items-center font-medium rounded-md text-[7px] leading-none text-xs md:btn md:btn-xs xl:btn-sm xl:p-2"
+							>
+								<span className="hidden md:block">DELETE</span>
+								<MdDeleteOutline className="md:hidden" />
 							</label>
 						</div>
 					)}
@@ -65,8 +72,8 @@ export const EmptyImageFrame = ({ content, date, prevPage }: ImageFrameDataType)
 					</figure>
 				)}
 			</Link>
-			<div className="card-body h-10 p-0 md:p-1 md:h-28">
-				<h3 className="card-title justify-center text-sm xl:text-md">{content}</h3>
+			<div className="md:h-20 xl:h-28 md:p-3">
+				<h3 className="card-title justify-center gap-1 leading-5 text-sm md:text-sm xl:text-lg whitespace-pre-wrap text-center">{content}</h3>
 			</div>
 		</div>
 	);
