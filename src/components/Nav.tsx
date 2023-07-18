6870ce257520fcc869d80b86c42d35ae9f8e9239
Kim Eunjeong <ryomi.j78@gmail.com>
2023-07-18 14:10:57 +0900

    use nav tag and adjust nav bar items position

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -18,72 +18,87 @@ export const Nav = () => {
 		localStorage.setItem("isLogin", "false");
 	};
 
-	return (
-		<div className="navbar absolute bg-base-100 justify-between">
-			<div className="navbar-start xxxs:w-full xl:w-1/2">
-				<div className="dropdown">
-					<label tabIndex={0} className="btn btn-ghost lg:hidden">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							className="h-5 w-5"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-						>
-							<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
-						</svg>
-					</label>
-					<ul
-						tabIndex={0}
-						className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 items-center"
-					>
-						<li className="font-medium">
-							<Link to="/talk">Talk</Link>
-						</li>
-						{isLogin && (
-							<>
-								<li className="font-medium">
-									<Link to="/TodayClothes">Today's Clothing</Link>
-								</li>
-								<li className="font-medium">
-									<Link to="/closet">My Closet</Link>
-								</li>
-							</>
-						)}
-						<li className="font-medium" onClick={handleLogout}>
-							{isLogin ? <Link to="/login">Logout</Link> : <Link to="/login">Login</Link>}
-						</li>
-					</ul>
-				</div>
-				<Link to="/" className="btn btn-ghost normal-case text-xl">
-					<div className="flex items-center gap-1">
-						<figure className="flex justify-center items-center w-7 h-7 rounded-full bg-sky-500">
-							<img src="/logo.svg" alt="logo" />
-						</figure>
-						<h1 className="xxxs:hidden xs:block">Today Clothing</h1>
-					</div>
-				</Link>
-			</div>
-			<div className="navbar-center hidden lg:flex">
-				<ul className="menu menu-horizontal px-1 items-center">
-					<li className="font-medium">
-						<Link to="/talk">Talk</Link>
-					</li>
-					{isLogin && (
-						<>
-							<li className="font-medium">
-								<Link to="/TodayClothes">Today's Clothing</Link>
-							</li>
-							<li className="font-medium">
-								<Link to="/closet">My Closet</Link>
-							</li>
-						</>
-					)}
-					<li className="font-medium" onClick={handleLogout}>
-						{isLogin ? <Link to="/login">Logout</Link> : <Link to="/login">Login</Link>}
-					</li>
-				</ul>
-			</div>
-		</div>
-	);
+  return (
+    <nav className="navbar absolute bg-base-100 justify-center">
+      <div className="flex gap-12 w-full max-w-5xl">
+        <div className="navbar-start xxxs:w-full xl:w-1/2">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 items-center"
+            >
+              <li className="font-medium">
+                <Link to="/talk">Talk</Link>
+              </li>
+              {isLogin && (
+                <>
+                  <li className="font-medium">
+                    <Link to="/TodayClothes">Today's Clothing</Link>
+                  </li>
+                  <li className="font-medium">
+                    <Link to="/closet">My Closet</Link>
+                  </li>
+                </>
+              )}
+              <li className="font-medium" onClick={handleLogout}>
+                {isLogin ? (
+                  <Link to="/login">Logout</Link>
+                ) : (
+                  <Link to="/login">Login</Link>
+                )}
+              </li>
+            </ul>
+          </div>
+          <Link to="/" className="btn btn-ghost normal-case text-xl">
+            <div className="flex items-center gap-1">
+              <figure className="flex justify-center items-center w-7 h-7 rounded-full bg-sky-500">
+                <img src="/logo.svg" alt="logo" />
+              </figure>
+              <h1 className="xxxs:hidden xs:block">Today Clothing</h1>
+            </div>
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 items-center">
+            <li className="font-medium">
+              <Link to="/talk">Talk</Link>
+            </li>
+            {isLogin && (
+              <>
+                <li className="font-medium">
+                  <Link to="/TodayClothes">Today's Clothing</Link>
+                </li>
+                <li className="font-medium">
+                  <Link to="/closet">My Closet</Link>
+                </li>
+              </>
+            )}
+            <li className="font-medium" onClick={handleLogout}>
+              {isLogin ? (
+                <Link to="/login">Logout</Link>
+              ) : (
+                <Link to="/login">Login</Link>
+              )}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
