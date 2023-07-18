6975ba45e9313c2087d91471366c1caab6ef4eac
Kim Eunjeong <ryomi.j78@gmail.com>
2023-07-18 16:26:23 +0900

    adjust image frame media query size

--- src/components/common/ImageFrame.tsx
+++ src/components/common/ImageFrame.tsx
@@ -18,20 +18,20 @@ export const ImageFrame = ({ content, post, deleteBtn, date, prevPage, handleCli
 	const [, setDate] = useRecoilState(selectedDate);
 
 	return (
-		<div className="card card-compact w-32 md:w-52 md:h-52 xl:w-96 xl:h-auto bg-base-100 shadow-xl">
-			<figure className="w-32 h-28 md:w-52 md:h-40 xl:w-96 xl:h-96 max-w-96 max-h-96 bg-base-200 cursor-pointer rounded-t-2xl overflow-hidden object-cover">
+		<div className="card card-compact w-full h-full bg-base-100 shadow-xl">
+			<figure className="w-full h-56 md:h-96 bg-base-200 cursor-pointer rounded-t-2xl overflow-hidden object-cover">
 				<img src={post?.imgUrl} alt={`${date?.toString().slice(0, 15)} clothing image`} />
 			</figure>
-			<div className="md:h-20 xl:h-28 md:p-3">
-				<h3 className="card-title justify-center gap-1 text-xs xl:text-lg whitespace-pre-wrap text-center">
+			<div className="h-20 md:h-28 p-3">
+				<h3 className="card-title justify-center gap-1 text-xs md:text-lg whitespace-pre-wrap text-center">
 					{content}
 				</h3>
 				<div className="flex md:gap-2 justify-end items-center mr-2">
-					<div className="card-actions justify-end xl:mt-0">
+					<div className="card-actions justify-end md:mt-0">
 						<Link to={`/editPost?prevPage=${prevPage}&content=${encodeURIComponent(content)}`}>
 							{date && (
 								<button
-									className="flex items-center font-medium rounded-md text-[7px] leading-none btn-xs md:btn md:btn-primary text-xs md:btn-xs xl:btn-sm xl:p-2"
+									className="flex items-center font-medium rounded-md text-[7px] leading-none btn-xs md:btn md:btn-primary text-xs md:btn-xs md:btn-sm md:p-2"
 									onClick={() => setDate(date.getTime())}
 								>
 									<span className="hidden md:block ">EDIT</span>
@@ -44,7 +44,7 @@ export const ImageFrame = ({ content, post, deleteBtn, date, prevPage, handleCli
 						<div className="form-control" onClick={() => handleClickPost(post)}>
 							<label
 								htmlFor="my-modal-6"
-								className="flex items-center font-medium rounded-md text-[7px] leading-none text-xs md:btn md:btn-xs xl:btn-sm xl:p-2"
+								className="flex items-center font-medium rounded-md text-[7px] leading-none text-xs md:btn md:btn-xs md:btn-sm md:p-2"
 							>
 								<span className="hidden md:block">DELETE</span>
 								<MdDeleteOutline className="md:hidden" />
@@ -61,18 +61,18 @@ export const EmptyImageFrame = ({ content, date, prevPage }: ImageFrameDataType)
 	const [, setDate] = useRecoilState(selectedDate);
 
 	return (
-		<div className="card card-compact w-32 md:w-52 md:h-52 xl:w-96 xl:h-auto bg-base-100 shadow-xl">
+		<div className="card card-compact w-full h-full bg-base-100 shadow-xl">
 			<Link to={`/editPost?prevPage=${prevPage}&content=${encodeURIComponent(content)}`}>
 				{date && (
 					<figure
-						className="w-32 h-28 md:w-52 md:h-40 xl:w-96 xl:h-96 max-w-96 max-h-96 bg-base-200 cursor-pointer rounded-t-2xl"
+					className="w-80 xs:w-full h-56 md:h-96 bg-base-200 cursor-pointer rounded-t-2xl overflow-hidden object-cover"
 						onClick={() => setDate(date.getTime())}
 					>
 						<img src="/addImg.svg" alt="No image data" className="w-1/4" />
 					</figure>
 				)}
 			</Link>
-			<div className="md:h-20 xl:h-28 md:p-3">
+			<div className="h-20 xl:h-28 md:p-3">
 				<h3 className="card-title justify-center gap-1 leading-5 text-sm md:text-sm xl:text-lg whitespace-pre-wrap text-center">{content}</h3>
 			</div>
 		</div>
