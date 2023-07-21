8000dded39d6f4e6cb9e1df605a5d69dd695f19b
Kim Eunjeong <ryomi.j78@gmail.com>
2023-07-21 11:08:43 +0900

    delete unnecessary codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Footer } from "./components/Footer";
-import { Nav } from "./components/Nav";
+import { Footer } from "./components/common/Footer";
+import { Nav } from "./components/common/Nav";
 import { SignUp } from "./views/SignUp";
 import { Record } from "./views/Record";
 import { EditPost } from "./views/EditPost";
@@ -17,67 +17,45 @@ const Login = lazy(() => import("./views/Login"));
 const TodayClothes = lazy(() => import("./views/TodayClothes"));
 
 function App() {
-	const isLogin = localStorage.getItem('isLogin') === null ? false : JSON.parse(localStorage.getItem('isLogin') || '')
-	const [, setUser] = useRecoilState(userInfo);
-	const [, setUserPosts] = useRecoilState(userPost);
-	const posts = useRecoilValue(postData);
+  const isLogin =
+    localStorage.getItem("isLogin") === null
+      ? false
+      : JSON.parse(localStorage.getItem("isLogin") || "");
+  const [, setUser] = useRecoilState(userInfo);
+  const [, setUserPosts] = useRecoilState(userPost);
+  const posts = useRecoilValue(postData);
 
-	useEffect(() => {
-		auth.onAuthStateChanged(async (user) => {
-			if (user) {
-				const c = await getUserData(user.uid);
-				if (c) {
-					setUser(c as unknown as User);
-					const userPosts = posts.filter((post) => post.uid === user.uid);
-					setUserPosts(userPosts);
-				}
-			}
-		});
-	}, []);
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const c = await getUserData(user.uid);
+        if (c) {
+          setUser(c as unknown as User);
+          const userPosts = posts.filter((post) => post.uid === user.uid);
+          setUserPosts(userPosts);
+        }
+      }
+    });
+  }, []);
 
-	return (
-		<BrowserRouter>
-			<Nav />
-			<main>
-				<Routes>
-					<Route
-						path="/*"
-						element={
-							isLogin ? (
-								<React.Suspense fallback={<div>Loading...</div>}>
-									<TodayClothes />
-								</React.Suspense>
-							) : (
-								<Talk />
-							)
-						}
-					/>
-					<Route
-						path="/login"
-						element={
-							<React.Suspense fallback={<div>Loading...</div>}>
-								<Login />
-							</React.Suspense>
-						}
-					/>
-					<Route
-						path="/todayClothes"
-						element={
-							<React.Suspense fallback={<div>Loading...</div>}>
-								<TodayClothes />
-							</React.Suspense>
-						}
-					/>
-					<Route path="/closet" element={<Closet />} />
-					<Route path="/talk" element={<Talk />} />
-					<Route path="/sign-up" element={<SignUp />} />
-					<Route path="/record" element={<Record />} />
-					<Route path="/editPost" element={<EditPost />} />
-				</Routes>
-			</main>
-			<Footer />
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Nav />
+      <main>
+        <Routes>
+          <Route path="/*" element={isLogin ? <TodayClothes /> : <Talk />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/todayClothes" element={<TodayClothes />} />
+          <Route path="/closet" element={<Closet />} />
+          <Route path="/talk" element={<Talk />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/record" element={<Record />} />
+          <Route path="/editPost" element={<EditPost />} />
+        </Routes>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
